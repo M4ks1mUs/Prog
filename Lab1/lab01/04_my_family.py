c7a45e0ce4334e623128529ca510dfa8863f88e9
Max <maksosip174@gmail.com>
2026-02-28 11:19:17 +0500

    fam

--- Lab1/lab01/04_my_family.py
+++ Lab1/lab01/04_my_family.py
@@ -16,11 +16,13 @@ my_family_height = [
     ['Макарий', 186],
     ['Максим', 184],
     ['Миша', 178],
-    ['Маша', 170]
+    ['Маша', 164]
 ]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
-print('Рост отца -', my_family_height[0][1])
+print(f'Рост отца - {my_family_height[0][1]} см')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+summ = my_family_height[0][1]+my_family_height[1][1]+my_family_height[2][1]+my_family_height[3][1]+my_family_height[4][1]+my_family_height[5][1]+my_family_height[6][1]
+print(f'Общий рост моей семьи - {summ} см')
