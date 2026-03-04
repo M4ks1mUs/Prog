6d364ec19a4477ee993d365975cf95c38743d7bf
Max <maksosip174@gmail.com>
2026-03-04 21:44:12 +0500

    1

--- Lab1/lab01/08_garden.py
+++ Lab1/lab01/08_garden.py
@@ -11,15 +11,50 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # garden_set =
 # meadow_set =
 # TODO здесь ваш код
+garden_set = []
+for i in garden:
+    if garden_set.count(i) == 0:
+        garden_set.append(i)
+
+meadow_set = []
+for i in meadow:
+    if meadow_set.count(i) == 0:
+        meadow_set.append(i)
+
+print(garden_set)
+print(meadow_set)
 
 # выведите на консоль все виды цветов
 # TODO здесь ваш код
+for i in garden_set:
+    print(i, end = " ")
+
+for i in meadow_set:
+    if garden_set.count(i) == 0:
+        print(i, end = " ")
+
+print()
 
 # выведите на консоль те, которые растут и там и там
 # TODO здесь ваш код
+for i in garden_set:
+    if meadow_set.count(i) == 1:
+        print(i, end = " ")
+
+print()
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
 # TODO здесь ваш код
+for i in garden_set:
+    if meadow_set.count(i) == 0:
+        print(i, end = " ")
+
+print()
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 # TODO здесь ваш код
+for i in meadow_set:
+    if garden_set.count(i) == 0:
+        print(i, end = " ")
+
+print()
