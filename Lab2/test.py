7c99dd7bd3f1b49302d9b4e9481e99d79f647080
Max <maksosip174@gmail.com>
2026-03-14 18:51:26 +0500

    urok1

--- Lab2/test.py
+++ Lab2/test.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
 import numpy as np
 
-# Линейная зависимость
-x = np.linspace(0, 10, 50)
-y1 = x
-# Квадратичная зависимость
-y2 = x**2
+x = np.linspace(0, 10, 10)
+y1 = 4*x
+y2 = [i**2 for i in x]
 
-# Построение графиков
-plt.figure(figsize=(9, 9)) # размер общего поля
+fig, ax = plt.subplots(figsize=(8, 6))
 
-plt.subplot(2, 1, 1) # местоположение поля с графиком
-plt.plot(x, y1) # построение графика
-plt.title('Зависимости: y1 = x, y2 = x^2') # заголовок
-plt.ylabel('y1', fontsize=14) # ось ординат
-plt.grid(True) # включение отображение сетки
+ax.set_title('Графики зависимостей: y1=4*x, y2=x^2', fontsize=16)
+ax.set_xlabel('x', fontsize=14)
+ax.set_ylabel('y1, y2', fontsize=14)
+ax.grid(which='major', linewidth=1.2)
+ax.grid(which='minor', linestyle='--', color='gray', linewidth=0.5)
+ax.scatter(x, y1, c='red', label='y1 = 4*x')
+ax.plot(x, y2, label='y2 = x^2')
+ax.legend()
 
-plt.subplot(2, 1, 2)
-plt.plot(x, y2) # построение графика
-plt.xlabel('x', fontsize=14) # ось абсцисс
-plt.ylabel('y2', fontsize=14) # ось ординат
-plt.grid(True) # включение отображение сетки
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+ax.tick_params(which='major', length=10, width=2)
+ax.tick_params(which='minor', length=5, width=1)
 
 plt.show()
